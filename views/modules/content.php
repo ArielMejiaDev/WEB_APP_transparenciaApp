a7da9833f7a4721771f003bff6bdf57fbb70ac65
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-04-26 16:45:32 -0600

    01 Archivos base

---
+++ views/modules/content.php
@@ -0,0 +1,7 @@
+<div class="be-content">
+  <div class="main-content container-fluid">
+    <div class="row">
+      
+    </div>
+  </div>
+</div>
---
+++ views/modules/sidebar.php
@@ -0,0 +1,47 @@
+<div class="be-left-sidebar">
+  <div class="left-sidebar-wrapper"><a href="#" class="left-sidebar-toggle">Dashboard</a>
+    <div class="left-sidebar-spacer">
+      <div class="left-sidebar-scroll">
+        <div class="left-sidebar-content">
+          <ul class="sidebar-elements">
+            <li class="divider">Menu</li>
+            <li class="active"><a href="index.html"><i class="icon mdi mdi-home"></i><span>Escritorio</span></a>
+            </li>
+            <li class="parent"><a href="charts.html"><i class="icon mdi mdi-account-circle"></i><span>Usuarios</span></a>
+              <ul class="sub-menu">
+                <li><a href="charts-flot.html"><i class="icon mdi mdi-account-add"></i> Crear usuario</a>
+                </li>
+                <li><a href="charts-sparkline.html"><i class="icon mdi mdi-accounts"></i> Lista de usuarios</a>
+                </li>
+              </ul>
+            </li>
+            <li class="parent"><a href="#"><i class="icon mdi mdi-collection-text"></i><span>Archivos</span></a>
+              <ul class="sub-menu">
+                <li><a href="form-elements.html"><i class="icon mdi mdi-cloud-upload"></i> Subir archivos</a>
+                </li>
+                <li><a href="form-validation.html"><i class="icon mdi mdi-check-all"></i> Publicar archivos</a>
+                </li>
+              </ul>
+            </li>
+            <li class="parent"><a href="#"><i class="icon mdi mdi-view-module"></i><span> Modulos</span></a>
+              <ul class="sub-menu">
+                <li><a href="tables-general.html"><i class="icon mdi mdi-collection-plus"></i> Crear Modulo</a>
+                </li>
+                <li><a href="tables-datatables.html"><i class="icon mdi mdi-plus-circle-o-duplicate"></i> Crear categoria</a>
+                </li>
+              </ul>
+            </li>
+            <li class="parent"><a href="#"><i class="icon mdi mdi-layers"></i><span>Departamentos</span></a>
+              <ul class="sub-menu">
+                <li><a href="pages-blank.html"><i class="icon mdi mdi-plus-circle"></i> Crear Depto</a>
+                </li>
+                <li><a href="pages-blank-header.html"><i class="icon mdi mdi-assignment-o"></i> Permisos</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
