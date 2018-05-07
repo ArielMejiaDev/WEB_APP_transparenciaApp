e8bf0e4a7609937e44c1f06b1dc0a70b980e2a59
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-05-07 16:41:29 -0600

    12 Se actualizo la base de datos

--- transparencia_app.sql
+++ transparencia_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2018 a las 00:33:16
+-- Tiempo de generación: 08-05-2018 a las 00:40:17
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
@@ -36,7 +36,7 @@ CREATE TABLE `usuarios` (
   `password` text NOT NULL,
   `email` text NOT NULL,
   `foto` text NOT NULL,
-  `rol` int(11) NOT NULL,
+  `rol` text NOT NULL,
   `intentos` int(11) NOT NULL,
   `pregunta_seguridad` text NOT NULL,
   `respuesta_seguridad` text NOT NULL
@@ -47,7 +47,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `usuario`, `password`, `email`, `foto`, `rol`, `intentos`, `pregunta_seguridad`, `respuesta_seguridad`) VALUES
-(1, 'Ariel Fernando', 'Salvador Mejia', 'asalvador', 'mvcpoophp7', 'asalvador@ipm.org.gt', 'https://lh3.googleusercontent.com/E_uwdhqJCgddahv-_WiptqiNgZl1HSIEBXaFXn3TzZ9ZWmhO_FT3S56uXXl5pdgNGw2_Mw2K-Sfbx0x2j3cM9S4h6MuMspTo36shFSRcZfCyziquDHVxk2_Onb6Nv4No8A5U0EkXFnQSCHNBcVcmUqPOpwvzjJYs8nhi-N-B4DuM92sBE2VQQ7lZxawa6MpDVMFOgOEnyK6uPZZ5lMuCK6YljIOqViMC9I4qILeM-xo8JvEmn_K9T3Wa047HtYUtUY3LllePpRfy9zdZj-wRhXchYYKGSv1trckCWETbbLtc7ZtslHk0fPxBMkO2K-A8Kff3I2obugNr4SMDenhWuCQ3r_oizfkODxyiWZ-gT8DR-8Ncm4CMGEhEqESUoYnBMUd3X5bCbj7V10T8TWEa6Z3ie8swmUQKH-1cL10oqiEZiNxvMsLBaLUjp8ZOZhdmDIDyEdsfOlmUGoRSt9MDgvPUMzJzPTN3OwJTw-TdjpY-z9MZrVSOTzwPpSuYqr7MEMZX7GVCbuBFGz_YXmsvUCPmgP2SYAjtRHx2FAa8oPa7G3ArdJFHHXOD_JkAluKDa6_SPeO-mDQeEqHM1abtE1RLvfYKTapaN7v9MQ=w218-h220-no', 2, 0, 'entendí esa ...', 'referencia');
+(1, 'Ariel Fernando', 'Salvador Mejia', 'asalvador', 'Mvcpoophp7', 'asalvador@ipm.org.gt', 'views/images/user.png', 'admin', 0, 'entendí esa ...', 'referencia'),
+(2, 'Otto Fernando', 'Salvador Mejia', 'osalvador', 'Av3ng3rs', 'osalvador@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'amarillo'),
+(4, 'David', 'de la Cruz', 'ddelacruz', 'Dcruz123456', 'ddelacruz@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'danger'),
+(5, 'Marlon', 'Melgar', 'mmelgar', 'Mm12345678', 'mmelgar@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'rojo');
 
 --
 -- Índices para tablas volcadas
@@ -67,7 +70,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
