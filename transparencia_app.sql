cc48ca68e1edaa0f198b17fe85d4aeee045e985a
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-05-15 16:38:46 -0600

    28 Se agrego la base de datos modificada para relacionar la tabla departamentos y usuarios

--- transparencia_app.sql
+++ transparencia_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-05-2018 a las 00:40:17
+-- Tiempo de generación: 16-05-2018 a las 00:37:02
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `id` int(11) NOT NULL,
+  `nombres` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `departamentos`
+--
+
+INSERT INTO `departamentos` (`id`, `nombres`) VALUES
+(1, 'Informatica'),
+(2, 'udaf');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -39,38 +58,61 @@ CREATE TABLE `usuarios` (
   `rol` text NOT NULL,
   `intentos` int(11) NOT NULL,
   `pregunta_seguridad` text NOT NULL,
-  `respuesta_seguridad` text NOT NULL
+  `respuesta_seguridad` text NOT NULL,
+  `id_departamento` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `usuario`, `password`, `email`, `foto`, `rol`, `intentos`, `pregunta_seguridad`, `respuesta_seguridad`) VALUES
-(1, 'Ariel Fernando', 'Salvador Mejia', 'asalvador', 'Mvcpoophp7', 'asalvador@ipm.org.gt', 'views/images/user.png', 'admin', 0, 'entendí esa ...', 'referencia'),
-(2, 'Otto Fernando', 'Salvador Mejia', 'osalvador', 'Av3ng3rs', 'osalvador@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'amarillo'),
-(4, 'David', 'de la Cruz', 'ddelacruz', 'Dcruz123456', 'ddelacruz@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'danger'),
-(5, 'Marlon', 'Melgar', 'mmelgar', 'Mm12345678', 'mmelgar@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'color', 'rojo');
+INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `usuario`, `password`, `email`, `foto`, `rol`, `intentos`, `pregunta_seguridad`, `respuesta_seguridad`, `id_departamento`) VALUES
+(9, 'Ariel Fernando', 'Salvador Mejia', 'asalvador', 'Mvcpoophp7', 'asalvador@ipm.org.gt', 'views/images/user.png', 'admin', 0, 'entendí esa ...', 'referencia', 1),
+(14, 'Roberto', 'Martinez', 'rmartinez', 'Mm123456', 'rmartinez@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'hola', 'mundo', 1),
+(30, 'Cesar', 'Vargas', 'cvargas', 'Cv123456', 'cvargas@ipm.org.gt', 'views/images/avatar.png', 'usuario', 0, 'hola', 'mundo', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_departamento` (`id_departamento`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
