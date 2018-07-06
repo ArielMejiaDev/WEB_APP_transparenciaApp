687e0b2c91b7c21e52a2056740d7553081d6a68e
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-07-06 08:48:43 -0600

    Se agrego validacion para el frontend y el backend para el formulario de activar docs

--- views/js/validarActivarDoc.js
+++ views/js/validarActivarDoc.js
@@ -1,11 +1,14 @@
 function validarActivarDoc(){
     var observaciones = document.getElementById('observaciones');
+    var avisoObservaciones = document.getElementById('avisoObservaciones');
     if (observaciones.value=='') {
         swal({
             type: 'error',
             title: 'Oops...',
             text: 'No puede quedar vacio',
         })
+        avisoObservaciones.style.display = "inline";
+        avisoObservaciones.innerHTML="No puede quedar vacio";
         return false;
     }
     return true;
