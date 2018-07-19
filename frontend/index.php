7222a1c9823e65c12e267454709b270616fe9607
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-07-19 09:25:52 -0600

    Se agrego el Modelo y Controlador para la vista Main y se eliminaron vistas innecesarias

--- frontend/index.php
+++ frontend/index.php
@@ -3,10 +3,12 @@
   include_once "controllers/TemplateController.php";
   include_once "controllers/EnlacesController.php";
   include_once "controllers/SidebarController.php";
+  include_once "controllers/MainController.php";
 // MODELS
   include_once "models/Conexion.php";
   include_once "models/EnlacesModel.php";
   include_once "models/SidebarModel.php";
+  include_once "models/MainModel.php";
 
   $template = new TemplateController();
   $template->includeTemplateController();
