24bcbe744bfdcd1eed194d5261d1483e406d8237
Ariel Salvador <arielsalvadormejia@gmail.com>
2018-07-26 16:18:55 -0600

    Se agrego la bd actualizada

--- backend/transparencia_app.sql
+++ backend/transparencia_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-07-2018 a las 20:46:47
+-- Tiempo de generación: 27-07-2018 a las 00:18:12
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
